c7049ca782cf1a1756979daed9e8bdb86532916c
Stephen <stephen@thoughtstem.com>
2018-05-11 16:52:10 -0700

    joints working

--- compiler.rkt
+++ compiler.rkt
@@ -1,6 +1,487 @@
 #lang racket
 
-(provide compile)
+(require (prefix-in h: 2htdp/image))
+
+(provide compile
+         make-static
+         make-dynamic
+         make-cosmetic
+         make-pivot
+         connect-pivot
+         
+         box-collider
+         circle-collider
+
+         initial-velocity
+
+         circle
+         rectangle
+         square
+
+         width
+         height
+
+         above
+         beside
+         overlay
+
+         preview
+         preview2
+         simulate)
+
+
+(define (map-with-i f l)
+  (for/list ([i (range (length l))])
+    (f (list-ref l i)
+       i)))
+
+(define (make-safe f)
+  (lambda (x . xs)
+    (if (empty? xs)
+        x
+        (apply f (cons x xs)))))
+
+(define (half i)
+  (/ i 2))
+
+
+(struct layout   (id x y w h name preview-f children)           #:transparent)
+(struct physical (id x y after-compile collider image dynamic?) #:transparent)
+(struct cosmetic (id x y after-compile image)                   #:transparent)
+
+(define (is-cosmetic? phys)
+  (cosmetic? phys))
+
+(define (is-dynamic? phys)
+  (and (physical? phys)
+       (physical-dynamic? phys)))
+
+(define (is-static? phys)
+  (and (physical? phys)
+       (not (physical-dynamic? phys))))
+
+(define/contract (get-image x)
+  (-> (or/c physical? cosmetic?) h:image?)
+  (if (physical? x)
+      (physical-image x)
+      (cosmetic-image x)))
+
+(define/contract (add-after-compile thing f)
+  (-> (or/c physical? cosmetic?) any/c (or/c physical? cosmetic?))
+  (cond [(physical? thing)  (struct-copy physical
+                                         thing
+                                         [after-compile (append (physical-after-compile thing) (list f))])]
+        [(cosmetic? thing)  (struct-copy cosmetic
+                                         thing
+                                         [after-compile (append (cosmetic-after-compile thing) (list f))])]))
+
+(struct collider ())
+(struct circle-collider collider (r))
+(struct box-collider collider (w h))
+
+
+
+(define current-id 0)
+(define (next-id)
+  (set! current-id (add1 current-id))
+  current-id)
+
+(define/contract (id thing)
+  (-> (or/c cosmetic? physical? layout?) number?)
+  (cond [(layout? thing)  (layout-id thing)]
+        [(physical? thing) (physical-id thing)]
+        [(cosmetic? thing)  (cosmetic-id thing)]))
+
+(define/contract (width thing)
+  (-> (or/c h:image? cosmetic? physical? layout?) number?)
+  (cond [(layout? thing)  (layout-w thing)]
+        [(h:image? thing) (h:image-width thing)]
+        [(physical? thing) (h:image-width (physical-image thing))]
+        [(cosmetic? thing)  (h:image-width (cosmetic-image thing))]))
+
+(define/contract (height thing)
+  (-> (or/c h:image? cosmetic? physical? layout?) number?)
+  (cond [(layout? thing)  (layout-h thing)]
+        [(h:image? thing) (h:image-height thing)]
+        [(physical? thing) (h:image-height (physical-image thing))]
+        [(cosmetic? thing)  (h:image-height (cosmetic-image thing))]))
+
+(define/contract (x thing)
+  (-> (or/c cosmetic? physical? layout?) number?)
+  (cond [(layout? thing)    (layout-x thing)]
+        [(physical? thing)  (physical-x thing)]
+        [(cosmetic? thing)  (cosmetic-x thing)]))
+
+(define/contract (y thing)
+  (-> (or/c cosmetic? physical? layout?) number?)
+  (cond [(layout? thing)    (layout-y thing)]
+        [(physical? thing)  (physical-y thing)]
+        [(cosmetic? thing)  (cosmetic-y thing)]))
+
+(define/contract (set-x thing new-x)
+  (-> (or/c cosmetic? physical? layout?)
+      number?
+      (or/c cosmetic? physical? layout?))
+
+  (cond [(layout? thing)    (struct-copy layout   thing  [x new-x])]
+        [(physical? thing)  (struct-copy physical thing  [x new-x])]
+        [(cosmetic? thing)  (struct-copy cosmetic thing  [x new-x])]))
+
+(define/contract (set-y thing new-y)
+  (-> (or/c cosmetic? physical? layout?)
+      number?
+      (or/c cosmetic? physical? layout?))
+
+  (cond [(layout? thing)    (struct-copy layout   thing  [y new-y])]
+        [(physical? thing)  (struct-copy physical thing  [y new-y])]
+        [(cosmetic? thing)  (struct-copy cosmetic thing  [y new-y])]))
+
+(define (set-xy thing new-x new-y)
+  (set-y (set-x thing new-x) new-y))
+
+(define (half-width t)
+  (half (width t)))
+
+(define (half-height t)
+  (half (height t)))
+
+
+(define (wrap-images i)
+  (if (h:image? i)
+      (make-cosmetic i)
+      i))
+
+;Takes in a list of things
+;  they are either images or physical objects
+(define/contract (overlay . things)
+  (->* () #:rest (listof (or/c h:image? cosmetic? physical? layout?)) layout?)
+
+  (define w (apply max (map width things)))
+  (define h (apply max (map height things)))
+  
+  (define (fix-xy thing i)
+    (define younger-siblings (take things i))
+
+
+    (define y-adj (if (layout? thing)
+                      (/ (- h (height thing)) 2)
+                      (/ h 2)))
+
+    (define x-adj
+      (if (layout? thing)
+          (/ (- w (width thing)) 2)
+          (/ w 2)))
+
+    (set-xy thing x-adj y-adj))
+
+  (layout (next-id)
+          0 0
+          w h
+          'overlay
+          h:overlay
+          (map-with-i fix-xy
+                      (map wrap-images things))))
+
+
+(define/contract (above . things)
+  (->* () #:rest (listof (or/c h:image? cosmetic? physical? layout?)) layout?)
+
+  (define w (apply max (map width things)))
+  (define h (apply + (map height things)))
+  
+  (define (fix-xy thing i)
+    (define younger-siblings (take things i))
+    (define y-adj (+ (y thing)
+                     (apply + (map height younger-siblings))))
+
+    (define x-adj
+      (if (layout? thing)
+          (/ (- w (width thing)) 2)
+          (/ w 2)))
+
+    (set-xy thing x-adj y-adj))
+  
+  (layout (next-id)
+          0 0
+          w h
+          'above
+          h:above
+          ;(map wrap-images things)
+          (map-with-i fix-xy
+                      (map wrap-images things))))
+
+
+(define/contract (beside . things)
+  (->* () #:rest (listof (or/c h:image? cosmetic? physical? layout?)) layout?)
+
+  (define w (apply + (map width things)))
+  (define h (apply max (map height things)))
+  
+  (define (fix-xy thing i)
+    (define younger-siblings (take things i))
+    (define y-adj (if (layout? thing)
+                      (/ (- h (height thing)) 2)
+                      (/ h 2)))
+
+    (define x-adj (+ (x thing)
+                     (apply + (map width younger-siblings))))
+
+    (set-xy thing x-adj y-adj))
+
+  (layout (next-id)
+          0 0
+          w h
+          'beside
+          h:beside
+          (map-with-i fix-xy
+                      (map wrap-images things))))
+
+(define/contract (preview thing)
+  (-> (or/c h:image? cosmetic? physical? layout?) h:image?)
+
+  (cond [(h:image? thing) thing]
+        [(cosmetic? thing) (cosmetic-image thing)]
+        [(physical? thing) (physical-image thing)]
+        [(layout? thing) (apply (make-safe (layout-preview-f thing))
+                                (map preview (layout-children thing)))]))
+
+
+
+(define/contract (collider-for i collider-type)
+  (-> h:image? any/c collider?)
+  (if (eq? collider-type circle-collider)
+      (circle-collider (max (h:image-width i)
+                            (h:image-height i)))
+
+      (box-collider (h:image-width i)
+                    (h:image-height i))))
+
+(define/contract (make-dynamic i #:collider (type circle-collider))
+  (->* ((or/c h:image? cosmetic? layout?)) (#:collider any/c) physical?)
+
+  (define img (preview i))
+  (physical (next-id)
+            (half-width img) (half-height img)
+            '()
+            (collider-for img circle-collider)
+            img
+            #t))
+
+(define/contract (make-static i #:collider (type circle-collider))
+  (->* ((or/c h:image? cosmetic? layout?)) (#:collider any/c) physical?)
+  (define img (preview i))
+  (physical (next-id) 
+            (half-width img) (half-height img)
+            '() ;After compile
+            (collider-for img type)
+            img
+            #f))
+
+(define/contract (make-cosmetic i)
+  (-> (or/c h:image? cosmetic? layout?) cosmetic?)
+  (define img (preview i))
+  (cosmetic (next-id)
+            (half-width img) (half-height img)
+            '()
+            img))
+
+(define/contract (make-pivot i)
+  (-> (or/c h:image? cosmetic?) cosmetic?)
+  (define base (make-cosmetic i))
+  (add-after-compile base
+                     (λ(me py-obj)
+                       (format "pivot~a = pivot((~a,~a))\npivots.append(pivot~a)"
+                               (id me)
+                               (pymunk-obj-x py-obj)
+                               (pymunk-obj-y py-obj)
+                               (id me)))))
+
+
+(define/contract (connect-pivot p other)
+  (-> cosmetic? physical? cosmetic?)
+  (add-after-compile p
+                     (λ(me py-obj)
+                       (format "pivot~a.connect(obj~a)"
+                               (id p)
+                               (id other)))))
+
+
+(define/contract (initial-velocity dir p)
+  (-> (listof number?) physical? physical?)
+  (add-after-compile p
+                     (λ(me py-obj)
+                       (format "obj~a.hit((~a, ~a), obj~a.position)"
+                               (id me)
+                               (first dir) (second dir)
+                               (id me)
+                               ))))
+
+
+
+
+(struct pymunk-obj (dynamic x y) #:transparent)
+(define (pymunk-obj-name o)
+  (format "obj~a" (id (pymunk-obj-dynamic o))))
+
+
+
+
+
+
+;Should interpret the layout structure into
+;  a list of colliders with absolute locations and appropriately bound images
+
+(define/contract (pymunk-obj-list current (root-x 0) (root-y 0))
+  (->* ((or/c physical? cosmetic? layout?))
+       (integer? integer?)
+       (listof pymunk-obj?))
+
+
+  (cond [(not (layout? current)) (list (pymunk-obj current
+                                               (+ root-x (x current))
+                                               (+ root-y (y current))))]
+        [(layout? current) (apply append
+                             (map-with-i (λ(c i)
+                                           (pymunk-obj-list c
+                                                            (+ root-x (x current))
+                                                            (+ root-y (y current))))
+                                         (layout-children current)))]))
+
+
+
+
+
+(define (obj->after-compiles x i)
+  (define phys (pymunk-obj-dynamic x))
+
+  (define after-compiles (if (cosmetic? phys)
+                             (cosmetic-after-compile phys)
+                             (physical-after-compile phys)))
+  
+  (string-join (map (λ(f) (f phys x)) after-compiles)
+               "\n"))
+
+
+(define (obj->python x i)
+  (define phys (pymunk-obj-dynamic x))
+
+  (define w (width  (pymunk-obj-dynamic x)))
+  (define h (height (pymunk-obj-dynamic x)))
+
+  (define after-compiles (if (cosmetic? phys)
+                             (cosmetic-after-compile phys)
+                             (physical-after-compile phys)))
+
+  (define collider (and
+                    (not (is-cosmetic? phys))
+                    (physical-collider phys)))
+
+  
+  (define collider-type (cond 
+                          [(circle-collider? collider) "ball"]
+                          [(box-collider? collider)    "box"]
+                          [else "ball"]))
+
+  
+  (define fun-name (format
+                    (cond [(is-cosmetic? phys) "cosmetic_~a"]
+                          [(is-dynamic? phys) "~a"]
+                          [(is-static? phys) "static_~a"])
+                    collider-type))
+
+  (define name
+    (pymunk-obj-name x))
+  
+  (define first-line
+    (if (box-collider? collider)
+        (format "~a = ~a((int(~a), int(~a)), ~a, ~a)"
+                name
+                fun-name
+                (- (pymunk-obj-x x) (half w))
+                (- (pymunk-obj-y x) (half h))
+                w
+                h)
+        (format "~a = ~a((int(~a), int(~a)), ~a)"
+                name
+                fun-name
+                (pymunk-obj-x x)
+                (pymunk-obj-y x)
+                (/ (max w h) 2))))
+
+  (define second-line
+    (format "~a.color = Color(~s)"
+            name
+            "white"))
+
+  (define third-line
+    (format "~a.group = ~a"
+            name
+            (id (pymunk-obj-dynamic x))))
+
+  ;Should be able to write out the embedded image here.  It's inside the dynamic/cosmetic object...
+  (define path
+    (format "/Users/thoughtstem/Dev/Python/py-fizzery/~a" (format "~a.png"
+                                                                  (pymunk-obj-name x))))
+
+  (h:save-image (get-image phys) path)
+  
+  (define fourth-line
+    (format "~a_image = pygame.image.load(~s)"
+            name
+            path))
+
+  (define fifth-line
+    (format "image_bindings.append([~a, ~a_image])"
+            name
+            name))
+
+  (define sixth-line
+    (format "user_shapes.append(~a)"
+            name))
+
+  (define construction-lines
+    (list first-line second-line third-line fourth-line fifth-line sixth-line))
+
+  (define after-compile-lines
+    (map force after-compiles))
+
+  (string-join construction-lines
+               "\n"))
+
+(define (simulate thing)
+  (define objs (reverse (pymunk-obj-list thing)))
+
+  (define pre (preview thing))
+  
+  (compile (string-join (append (map-with-i obj->python objs)
+                                (map-with-i obj->after-compiles objs)
+                                ) "\n")
+           "demo.py"
+           (h:image-width pre)
+           (h:image-height pre)))
+
+(define (preview2 thing)
+  (define objs (pymunk-obj-list thing))
+
+  (define pre (preview thing))
+  
+  (define bg (h:rectangle (h:image-width pre)
+                          (h:image-height pre)
+                          "solid"
+                          "transparent"))
+
+  (apply (make-safe h:overlay)
+         (map (λ(i) (h:place-image
+                     (get-image (pymunk-obj-dynamic i))
+                     (pymunk-obj-x i)
+                     (pymunk-obj-y i)
+                     bg)) objs)))
+
+
+(define circle    (compose make-cosmetic h:circle))
+(define square    (compose make-cosmetic h:square))
+(define rectangle (compose make-cosmetic h:rectangle))
+
 
 (define (program->string p)
   (~a p))
@@ -42,11 +523,10 @@ window('Shape Methods & Properties', ~a, ~a)
 
 user_shapes = []
 image_bindings = []
+pivots = []
 
 ~a
 
-#floor = static_box((0, 590), 600, 10)
-#floor.elasticity = 0.0
 
 add_observer(hit_ball)
 
@@ -61,7 +541,7 @@ def image_for(s):
       return b[1]
   return False
 
-def test(keys):
+def draw_images(keys):
   global user_shapes
   for s in user_shapes:
     if(not image_for(s)):
@@ -85,8 +565,22 @@ def test(keys):
     screen = pygame.display.get_surface()
     screen.blit(rotated_logo_img, p)
 
+def draw_pivot_lines(keys):
+  global pivots
+  for p in pivots:
+    start = p.body.position
+    for j in p.shape:
+      other = j.a
+      end = other.position
+      
+      screen = pygame.display.get_surface()
+      pygame.draw.line(screen, Color(\"black\"), start, end)
 
-add_observer(test)
+      #screen.blit(rotated_logo_img, p)
+
+
+add_observer(draw_images)
+add_observer(draw_pivot_lines)
 
 run()"
   )
--- demo.py
+++ demo.py
@@ -17,68 +17,186 @@ def hit_ball(keys):
 
 
 
-window('Shape Methods & Properties', 320, 320)
+window('Shape Methods & Properties', 505, 373)
 
 user_shapes = []
 image_bindings = []
+pivots = []
 
-obj11_0 = static_box((10, 310), 300, 10)
-obj11_0.color = Color("white")
-obj11_0.group = 11
-obj11_0_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj11.png")
-image_bindings.append([obj11_0, obj11_0_image])
-user_shapes.append(obj11_0)
-obj13_1 = static_box((310, 10), 10, 300)
-obj13_1.color = Color("white")
-obj13_1.group = 13
-obj13_1_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj13.png")
-image_bindings.append([obj13_1, obj13_1_image])
-user_shapes.append(obj13_1)
-obj8_2 = cosmetic_ball((160, 160), 150)
-obj8_2.color = Color("white")
-obj8_2.group = 8
-obj8_2_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj8.png")
-image_bindings.append([obj8_2, obj8_2_image])
-user_shapes.append(obj8_2)
-obj6_3 = ball((160, 185), 50)
-obj6_3.color = Color("white")
-obj6_3.group = 6
-obj6_3_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj6.png")
-image_bindings.append([obj6_3, obj6_3_image])
-user_shapes.append(obj6_3)
-obj3_4 = ball((210, 110), 25)
-obj3_4.color = Color("white")
-obj3_4.group = 3
-obj3_4_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj3.png")
-image_bindings.append([obj3_4, obj3_4_image])
-user_shapes.append(obj3_4)
-obj2_5 = ball((160, 110), 25)
-obj2_5.color = Color("white")
-obj2_5.group = 2
-obj2_5_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj2.png")
-image_bindings.append([obj2_5, obj2_5_image])
-user_shapes.append(obj2_5)
-obj1_6 = ball((110, 110), 25)
-obj1_6.color = Color("white")
-obj1_6.group = 1
-obj1_6_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj1.png")
-image_bindings.append([obj1_6, obj1_6_image])
-user_shapes.append(obj1_6)
-obj13_7 = static_box((0, 10), 10, 300)
-obj13_7.color = Color("white")
-obj13_7.group = 13
-obj13_7_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj13.png")
-image_bindings.append([obj13_7, obj13_7_image])
-user_shapes.append(obj13_7)
-obj11_8 = static_box((10, 0), 300, 10)
-obj11_8.color = Color("white")
-obj11_8.group = 11
-obj11_8_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj11.png")
-image_bindings.append([obj11_8, obj11_8_image])
-user_shapes.append(obj11_8)
+obj30 = static_box((int(0), int(363)), 505, 10)
+obj30.color = Color("white")
+obj30.group = 30
+obj30_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj30.png")
+image_bindings.append([obj30, obj30_image])
+user_shapes.append(obj30)
+obj32 = static_box((int(495), int(10)), 10, 353)
+obj32.color = Color("white")
+obj32.group = 32
+obj32_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj32.png")
+image_bindings.append([obj32, obj32_image])
+user_shapes.append(obj32)
+obj28 = cosmetic_ball((int(505/2), int(373/2)), 485/2)
+obj28.color = Color("white")
+obj28.group = 28
+obj28_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj28.png")
+image_bindings.append([obj28, obj28_image])
+user_shapes.append(obj28)
+obj22 = ball((int(755/2), int(455/2)), 25)
+obj22.color = Color("white")
+obj22.group = 22
+obj22_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj22.png")
+image_bindings.append([obj22, obj22_image])
+user_shapes.append(obj22)
+obj23 = cosmetic_ball((int(755/2), int(355/2)), 25)
+obj23.color = Color("white")
+obj23.group = 23
+obj23_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj23.png")
+image_bindings.append([obj23, obj23_image])
+user_shapes.append(obj23)
+obj21 = cosmetic_ball((int(755/2), int(273/2)), 16)
+obj21.color = Color("white")
+obj21.group = 21
+obj21_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj21.png")
+image_bindings.append([obj21, obj21_image])
+user_shapes.append(obj21)
+obj18 = ball((int(655/2), int(455/2)), 25)
+obj18.color = Color("white")
+obj18.group = 18
+obj18_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj18.png")
+image_bindings.append([obj18, obj18_image])
+user_shapes.append(obj18)
+obj19 = cosmetic_ball((int(655/2), int(355/2)), 25)
+obj19.color = Color("white")
+obj19.group = 19
+obj19_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj19.png")
+image_bindings.append([obj19, obj19_image])
+user_shapes.append(obj19)
+obj17 = cosmetic_ball((int(655/2), int(273/2)), 16)
+obj17.color = Color("white")
+obj17.group = 17
+obj17_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj17.png")
+image_bindings.append([obj17, obj17_image])
+user_shapes.append(obj17)
+obj14 = ball((int(555/2), int(455/2)), 25)
+obj14.color = Color("white")
+obj14.group = 14
+obj14_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj14.png")
+image_bindings.append([obj14, obj14_image])
+user_shapes.append(obj14)
+obj15 = cosmetic_ball((int(555/2), int(355/2)), 25)
+obj15.color = Color("white")
+obj15.group = 15
+obj15_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj15.png")
+image_bindings.append([obj15, obj15_image])
+user_shapes.append(obj15)
+obj13 = cosmetic_ball((int(555/2), int(273/2)), 16)
+obj13.color = Color("white")
+obj13.group = 13
+obj13_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj13.png")
+image_bindings.append([obj13, obj13_image])
+user_shapes.append(obj13)
+obj10 = ball((int(455/2), int(455/2)), 25)
+obj10.color = Color("white")
+obj10.group = 10
+obj10_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj10.png")
+image_bindings.append([obj10, obj10_image])
+user_shapes.append(obj10)
+obj11 = cosmetic_ball((int(455/2), int(355/2)), 25)
+obj11.color = Color("white")
+obj11.group = 11
+obj11_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj11.png")
+image_bindings.append([obj11, obj11_image])
+user_shapes.append(obj11)
+obj9 = cosmetic_ball((int(455/2), int(273/2)), 16)
+obj9.color = Color("white")
+obj9.group = 9
+obj9_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj9.png")
+image_bindings.append([obj9, obj9_image])
+user_shapes.append(obj9)
+obj6 = ball((int(355/2), int(455/2)), 25)
+obj6.color = Color("white")
+obj6.group = 6
+obj6_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj6.png")
+image_bindings.append([obj6, obj6_image])
+user_shapes.append(obj6)
+obj7 = cosmetic_ball((int(355/2), int(355/2)), 25)
+obj7.color = Color("white")
+obj7.group = 7
+obj7_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj7.png")
+image_bindings.append([obj7, obj7_image])
+user_shapes.append(obj7)
+obj5 = cosmetic_ball((int(355/2), int(273/2)), 16)
+obj5.color = Color("white")
+obj5.group = 5
+obj5_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj5.png")
+image_bindings.append([obj5, obj5_image])
+user_shapes.append(obj5)
+obj2 = ball((int(255/2), int(455/2)), 25)
+obj2.color = Color("white")
+obj2.group = 2
+obj2_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj2.png")
+image_bindings.append([obj2, obj2_image])
+user_shapes.append(obj2)
+obj3 = cosmetic_ball((int(255/2), int(355/2)), 25)
+obj3.color = Color("white")
+obj3.group = 3
+obj3_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj3.png")
+image_bindings.append([obj3, obj3_image])
+user_shapes.append(obj3)
+obj1 = cosmetic_ball((int(255/2), int(273/2)), 16)
+obj1.color = Color("white")
+obj1.group = 1
+obj1_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj1.png")
+image_bindings.append([obj1, obj1_image])
+user_shapes.append(obj1)
+obj32 = static_box((int(0), int(10)), 10, 353)
+obj32.color = Color("white")
+obj32.group = 32
+obj32_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj32.png")
+image_bindings.append([obj32, obj32_image])
+user_shapes.append(obj32)
+obj30 = static_box((int(0), int(0)), 505, 10)
+obj30.color = Color("white")
+obj30.group = 30
+obj30_image = pygame.image.load("/Users/thoughtstem/Dev/Python/py-fizzery/obj30.png")
+image_bindings.append([obj30, obj30_image])
+user_shapes.append(obj30)
 
-#floor = static_box((0, 590), 600, 10)
-#floor.elasticity = 0.0
+
+
+obj22.hit((-500000, 0), obj22.position)
+
+pivot21 = pivot((755/2,273/2))
+pivots.append(pivot21)
+pivot21.connect(obj22)
+obj18.hit((-500000, 0), obj18.position)
+
+pivot17 = pivot((655/2,273/2))
+pivots.append(pivot17)
+pivot17.connect(obj18)
+obj14.hit((0, 0), obj14.position)
+
+pivot13 = pivot((555/2,273/2))
+pivots.append(pivot13)
+pivot13.connect(obj14)
+obj10.hit((0, 0), obj10.position)
+
+pivot9 = pivot((455/2,273/2))
+pivots.append(pivot9)
+pivot9.connect(obj10)
+obj6.hit((0, 0), obj6.position)
+
+pivot5 = pivot((355/2,273/2))
+pivots.append(pivot5)
+pivot5.connect(obj6)
+obj2.hit((0, 0), obj2.position)
+
+pivot1 = pivot((255/2,273/2))
+pivots.append(pivot1)
+pivot1.connect(obj2)
+
+
+
 
 add_observer(hit_ball)
 
@@ -93,7 +211,7 @@ def image_for(s):
       return b[1]
   return False
 
-def test(keys):
+def draw_images(keys):
   global user_shapes
   for s in user_shapes:
     if(not image_for(s)):
@@ -117,7 +235,21 @@ def test(keys):
     screen = pygame.display.get_surface()
     screen.blit(rotated_logo_img, p)
 
+def draw_pivot_lines(keys):
+  global pivots
+  for p in pivots:
+    start = p.body.position
+    for j in p.shape:
+      other = j.a
+      end = other.position
+      
+      screen = pygame.display.get_surface()
+      pygame.draw.line(screen, Color("black"), start, end)
 
-add_observer(test)
+      #screen.blit(rotated_logo_img, p)
+
+
+add_observer(draw_images)
+add_observer(draw_pivot_lines)
 
 run()
