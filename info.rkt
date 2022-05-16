4a11bbf288f55bc25a28ac7182f303abb41e24e9
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-05-16 16:27:54 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,9 @@
 #lang info
 (define version "0.0.1")
 
-(define deps '("racket-to-python"))
+(define deps '("base"
+               "htdp-lib"
+               "racket-to-python"))
 (define scribblings '(("py-fizz.scrbl" ())))
+(define build-deps '("scribble-lib"
+                     "wxme-lib"))
